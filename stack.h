49ff40edb749432db409c0a99116e2b769441c14
srsaidas <mail2saidas@gmai.com>
2020-04-17 09:43:34 +0530

    Stack prototype

--- stack.h
+++ stack.h
@@ -1,43 +1,4 @@
-#define STACKSIZE 5
-int stack[STACKSIZE];
-int top = -1 // Top of the stack
-void push(int element)
-{//
-		if(top == STACKSIZE -1 )
-		{
-				printf("Stack is full\n")
-		}
-		else
-		{
-				stack[++top] = element;
-		}
-}
+#ifndef STACK_H
+#define STACK_H
 
-int pull(void)
-{//
-		if(top == -1)
-		{
-				printf("Stack is Empty");
-		}
-		else
-		{
-				return stack[top--];
-		}
-}
-
-void traverse(void)
-{//
-		if(top == -1)
-		{
-				printf("Stack is Empty");
-		}
-		else
-		{
-				int i;
-				for(i = 0; i <= top; i++)
-				{
-						printf("%d\n", stack[i]);
-				}
-		}
-}
-
+#endif
